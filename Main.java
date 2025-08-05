84bd477458f6e4a731fc7f95c5bca531aada1e08
JAYASRIRAM-N <sriram29134@gmail.com>
2025-08-05 15:17:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare variables
+        int age = 25;
+        String name = "Aarav";
+        double height = 5.9;
+        boolean isStudent = true;
+
+        // Print variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Is a student? " + isStudent);
+    }
+}
